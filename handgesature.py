2ed51cb79cc8c8a6e50e811c1700f0d8f37b1467
KANDULA JITHENDRA SUBRAMANYAM <72296574+jithendra259@users.noreply.github.com>
2024-10-18 15:40:27 +0530

    Update handgesature.py

--- handgesature.py
+++ handgesature.py
@@ -1,1 +1,27 @@
-print("hello")
+import cv2
+import mediapipe as mp 
+import time  
+cap =cv2.VideoCapture(1)
+
+mpHands=mp.solutions.hands 
+hands=mpHands.Hands()
+
+
+
+
+
+while True: 
+    success, img =cap.read()
+    
+    
+    imgRGB= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results=hands.process(imgRGB)
+    print(results)
+    print(results.multi_hand_landmarks)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            
+    
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
